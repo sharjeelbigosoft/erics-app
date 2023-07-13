d838436c6e4c43d27cd3d766b6faa8076c016941
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2023-07-13 15:44:03 +0500

    Services data gathering.

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,49 +1,66 @@
 import React from 'react'
 
 export default function Services() {
+  const [oil, setOil]=React.useState()
+  const [brake, setBrake]=React.useState()
+  const [AC, setAC]=React.useState()
+  const [tyre, setTyre]=React.useState()
+  const [steering, setSteering]=React.useState()
+  const [maintenance, setMaintenance]=React.useState()
+  const [engine, setEngine]=React.useState()
+  const [inspection, setInspection]=React.useState()
+
+  const oil_=React.useRef()
+  const brake_=React.useRef()
+  const AC_=React.useRef()
+  const tyre_=React.useRef()
+  const steering_=React.useRef()
+  const maintenance_=React.useRef()
+  const engine_=React.useRef()
+  const inspection_=React.useRef()
   return (
     <div className='flex justify-center'>
         <div className='lg:w-1/2'>
-          <div className='w-full'>
-            <input type="checkbox" id="oil" name="oil"/>
-            <label for="oil">Oil Change</label>
+          <div className=''>
+            <input onClick={e=>{setOil(oil=>e.target.checked);}} type="checkbox" id="oil" name="oil"/>
+            <label htmlFor="oil">Oil Change</label>
           </div>
-          <textarea className='border rounded-md w-full' rows={5}></textarea>
+          <textarea onChange={e=>{oil_.current=e.target.value;}} className='border rounded-md w-full' rows={5}></textarea>
           <div>
-            <input type="checkbox" id="brake" name="brake"/>
-            <label for="brake">Brake Service</label>
+            <input onClick={e=>{setBrake(brake=>e.target.checked);}} type="checkbox" id="brake" name="brake"/>
+            <label htmlFor="brake">Brake Service</label>
           </div>
-          <textarea className='border rounded-md w-full' rows={5}></textarea>
+          <textarea onChange={e=>{brake_.current=e.target.value;}}  className='border rounded-md w-full' rows={5}></textarea>
           <div>
-            <input type="checkbox" id="ac" name="ac"/>
-            <label for="ac">AC & Heating</label>
+            <input onClick={e=>{setAC(AC=>e.target.checked);}} type="checkbox" id="ac" name="ac"/>
+            <label htmlFor="ac">AC & Heating</label>
           </div>
-          <textarea className='border rounded-md w-full' rows={5}></textarea>
+          <textarea onChange={e=>{AC_.current=e.target.value;}}  className='border rounded-md w-full' rows={5}></textarea>
           <div>
-            <input type="checkbox" id="tyre" name="tyre"/>
-            <label for="tyre">Tire Change</label>
+            <input onClick={e=>{setTyre(tyre=>e.target.checked);}} type="checkbox" id="tyre" name="tyre"/>
+            <label htmlFor="tyre">Tire Change</label>
           </div>
-          <textarea className='border rounded-md w-full' rows={5}></textarea>
+          <textarea onChange={e=>{tyre_.current=e.target.value;}}  className='border rounded-md w-full' rows={5}></textarea>
           <div>
-            <input type="checkbox" id="steering" name="steering"/>
-            <label for="steering">Steering Repair</label>
+            <input onClick={e=>{setSteering(steering=>e.target.checked);}} type="checkbox" id="steering" name="steering"/>
+            <label htmlFor="steering">Steering Repair</label>
           </div>
-          <textarea className='border rounded-md w-full' rows={5}></textarea>
+          <textarea onChange={e=>{steering_.current=e.target.value;}}  className='border rounded-md w-full' rows={5}></textarea>
           <div>
-            <input type="checkbox" id="maintenance" name="maintenance"/>
-            <label for="maintenance">General Maintenance</label>
+            <input onClick={e=>{setMaintenance(maintenance=>e.target.checked);}} type="checkbox" id="maintenance" name="maintenance"/>
+            <label htmlFor="maintenance">General Maintenance</label>
           </div>
-          <textarea className='border rounded-md w-full' rows={5}></textarea>
+          <textarea onChange={e=>{maintenance_.current=e.target.value;}}  className='border rounded-md w-full' rows={5}></textarea>
           <div>
-            <input type="checkbox" id="engine" name="engine"/>
-            <label for="engine">Diagnostic / Check engine light</label>
+            <input onClick={e=>{setEngine(engine=>e.target.checked);}} type="checkbox" id="engine" name="engine"/>
+            <label htmlFor="engine">Diagnostic / Check engine light</label>
           </div>
-          <textarea className='border rounded-md w-full' rows={5}></textarea>
+          <textarea onChange={e=>{engine_.current=e.target.value;}}  className='border rounded-md w-full' rows={5}></textarea>
           <div>
-            <input type="checkbox" id="inspection" name="inspection"/>
-            <label for="inspection">State Inspection</label>
+            <input onClick={e=>{setInspection(inspection=>e.target.checked);}} type="checkbox" id="inspection" name="inspection"/>
+            <label htmlFor="inspection">State Inspection</label>
           </div>
-          <textarea className='border rounded-md w-full' rows={5}></textarea>
+          <textarea onChange={e=>{inspection_.current=e.target.value;}}  className='border rounded-md w-full' rows={5}></textarea>
         </div>
     </div>
   )
